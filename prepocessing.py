f474290e8a6403ee2c198e85a1850f7250ee13a3
Matteo Costantini <matteo.angelo.costantini@gmail.com>
2018-06-03 21:14:04 +0200

    Spostato preprocessing in preprocessing.py

--- prepocessing.py
+++ prepocessing.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
-import re, io
+import re, io, os
 import pandas as pd
+import numpy as np
 from datetime import datetime
 from IPython import embed
+import smarthouse
 
+
+pd.set_option('display.expand_frame_repr', False)
 
 # Conversione del dataset da file .txt a file .csv
 def sensor_data_to_csv(path):
@@ -24,18 +28,26 @@ def date_to_timestamp(m):
     )[:-2] # Perché non mi piaceva vedere .0
 
 
+def find_state(timestamp, states):
+    l = np.asarray(states)
+    idx = (np.abs(l - timestamp)).argmin()
+    return states[idx]
+
+
 def main():
-    # Conversione txt -> csv e conversione date in timestamp
     files = [
         'OrdonezA_ADLs',
-        'OrdonezB_ADLs',
         'OrdonezA_Sensors',
+        'OrdonezB_ADLs',
         'OrdonezB_Sensors',
     ]
     fieldnames_ADL = ['start_time', 'end_time', 'activity']
     fieldnames_sensors = ['start_time', 'end_time', 'location','type', 'place']
+    dfs = {}
 
+    os.system('rm dataset_csv/*')
     for f in files:
+        # Conversione txt -> csv e conversione date in timestamp
         out = io.StringIO()
         if f.find('ADL') > 0:
             out.write(','.join(fieldnames_ADL))
@@ -50,6 +62,38 @@ def main():
         df.sort_values(by=['start_time'], inplace=True)
         df.drop(df[df['start_time'] > df['end_time']].index, inplace=True)
         df.to_csv(f'dataset_csv/{f}.csv', index=False)
+        df.reset_index(inplace=True)
+        dfs[f] = df
+
+    for i in range(2):
+        adl = dfs[files[2 * i]]
+        obs = dfs[files[2 * i + 1]]
+
+        # Calcolo timestamp medio di osservazioni e stati
+        adl['mean_timestamp'] = (adl['start_time'] + adl['end_time']) // 2
+        obs['mean_timestamp'] = (obs['start_time'] + obs['end_time']) // 2
+
+        # Ad ogni osservazione di un sensore viene associato l'evento più vicino
+        obs['state_timestamp'] = 0
+        for idx, row in obs.iterrows():
+            state_timestamp = find_state(row[6], adl['mean_timestamp'])
+            obs.at[idx, 'state_timestamp'] = state_timestamp
+
+        merged = pd.merge(obs, adl,
+            left_on='state_timestamp',right_on='mean_timestamp'
+        )
+
+        # Drop colonne inutili e rename
+        merged.drop(columns=['state_timestamp', 'index_x', 'index_y'],
+            axis=1, inplace=True)
+        merged.columns = ['start_time_sensor', 'end_time_sensor', 'location',
+            'type', 'place', 'mean_sensor', 'start_time_activity',
+            'end_time_activity', 'activity', 'mean_activity'
+        ]
+
+        merged.to_csv(f'dataset_csv/full_{"A" if i == 0 else "B"}.csv',
+            sep=',', index=False)
+
 
 
 if __name__ == '__main__':
--- smarthouse.py
+++ smarthouse.py
@@ -3,35 +3,8 @@ import numpy as np
 
 pd.set_option('display.expand_frame_repr', False)
 
-def find_state(timestamp, states):
-    l = np.asarray(states)
-    idx = (np.abs(l - timestamp)).argmin()
-    return states[idx]
-
+def main():
+    pass
 
 if __name__ == '__main__':
-    datasets_state = ['OrdonezA_ADLs.csv', 'OrdonezB_ADLs.csv']
-    datasets_obs = ['OrdonezA_Sensors.csv', 'OrdonezB_Sensors.csv']
-
-    states = pd.read_csv(f'dataset_csv/{datasets_state[0]}')
-    obs = pd.read_csv(f'dataset_csv/{datasets_obs[0]}')
-
-    states['mean_timestamp'] = (states['start_time'] + states['end_time']) // 2
-    obs['mean_timestamp'] = (obs['start_time'] + obs['end_time']) // 2
-
-    obs['state_timestamp'] = 0
-    for i, row in obs.iterrows():
-        obs.set_value(i, 'state_timestamp',
-            find_state(row[5], states['mean_timestamp'])
-        )
-
-    merged = pd.merge(obs, states,
-        left_on='state_timestamp',right_on='mean_timestamp'
-    )
-    merged.drop(columns=['state_timestamp'], axis=1, inplace=True)
-    merged.columns = ['start_time_sensor', 'end_time_sensor', 'location',
-        'type', 'place', 'mean_sensor', 'start_time_activity',
-        'end_time_activity', 'activity', 'mean_activity'
-    ]
-
-    merged.to_csv('dataset_csv/full.csv', sep=',', index=False)
+    main()
