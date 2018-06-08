f46d2635bd73ca49d5a8e3c7c30c4e6f20014e96
Matteo Costantini <matteo.angelo.costantini@gmail.com>
2018-06-08 16:10:55 +0200

    Abbiamo sbagliato qualcosa...

--- smarthouse.py
+++ smarthouse.py
@@ -52,40 +52,111 @@ def main():
 
         # TODO: Suddividere in train e test set
         size = int(df.shape[0]*0.8)
-        trainset_s = df['activity'][:size]; testset_s = df['activity'][size:]
-        trainset_o = df['sensors'][:size]; testset_o = df['sensors'][size:]
+        trainset_s = df['activity'][:size]; testset_s = df['activity'].tolist()[size:]
+        trainset_o = df['sensors'][:size]; testset_o = df['sensors'].tolist()[size:]
 
         P = prior(trainset_s)
         T = transition_matrix(trainset_s)
         O = obs_matrix(trainset_s, trainset_o)
 
-        model = HiddenMarkovModel(name=f"model{f}")
-        # Inizializzazione gli stati
-        states = []
-        for i in range(O.shape[0]):
-            d = dict(enumerate(O[i,:]))
-            states.append(State(DiscreteDistribution(d), name=f'{i}'))
-        model.add_states(*states)
+        x, T1, T2 = viterbi(testset_o, T, O, P)
+        c = 0
+        for i, j in zip(x, testset_s):
+            if i == j:
+                c += 1
+        print(c/len(x))
 
-        # Definizione delle probabilità iniziali
-        for i, p in enumerate(P):
-            model.add_transition(model.start, states[i], p)
+    #     model = HiddenMarkovModel(name=f"model{f}")
+    #     # Inizializzazione gli stati
+    #     states = []
+    #     for i in range(O.shape[0]):
+    #         d = dict(enumerate(O[i,:]))
+    #         states.append(State(DiscreteDistribution(d), name=f'{i}'))
+    #     model.add_states(*states)
 
-        # Definizione delle transizioni tra stati
-        for i, s1 in enumerate(states):
-            for j, s2 in enumerate(states):
-                model.add_transition(s1, s2, T[i, j])
+    #     # Definizione delle probabilità iniziali
+    #     for i, p in enumerate(P):
+    #         model.add_transition(model.start, states[i], p)
 
-    #           (
-    #            )
-    #       __..---..__
-    #   ,-='  /  |  \  `=-.
-    #  :--..___________..--;
-    #   \.,_____________,./
-        model.bake()
+    #     # Definizione delle transizioni tra stati
+    #     for i, s1 in enumerate(states):
+    #         for j, s2 in enumerate(states):
+    #             model.add_transition(s1, s2, T[i, j])
 
+    # #           (
+    # #            )
+    # #       __..---..__
+    # #   ,-='  /  |  \  `=-.
+    # #  :--..___________..--;
+    # #   \.,_____________,./
+    #     model.bake()
+    #     # import pdb; pdb.set_trace()
+    #     res = model.viterbi(testset_o)
+    #     seq = [i for i, s in res[1]]
+    #     seq = seq[1:]
+    #     c = 0
+    #     for i, j in zip(seq, testset_s):
+    #         if i == j:
+    #             c += 1
+
+    #     print(c/len(seq))
 
     return P, T, O
+
+
+def viterbi(y, A, B, Pi=None):
+    """
+    Return the MAP estimate of state trajectory of Hidden Markov Model.
+
+    Parameters
+    ----------
+    y : array (T,)
+        Observation state sequence. int dtype.
+    A : array (K, K)
+        State transition matrix. See HiddenMarkovModel.state_transition  for
+        details.
+    B : array (K, M)
+        Emission matrix. See HiddenMarkovModel.emission for details.
+    Pi: optional, (K,)
+        Initial state probabilities: Pi[i] is the probability x[0] == i. If
+        None, uniform initial distribution is assumed (Pi[:] == 1/K).
+
+    Returns
+    -------
+    x : array (T,)
+        Maximum a posteriori probability estimate of hidden state trajectory,
+        conditioned on observation sequence y under the model parameters A, B,
+        Pi.
+    T1: array (K, T)
+        the probability of the most likely path so far
+    T2: array (K, T)
+        the x_j-1 of the most likely path so far
+    """
+    # Cardinality of the state space
+    K = A.shape[0]
+    # Initialize the priors with default (uniform dist) if not given by caller
+    Pi = Pi if Pi is not None else np.full(K, 1 / K)
+    T = len(y)
+    T1 = np.empty((K, T), 'd')
+    T2 = np.empty((K, T), 'B')
+
+    # Initilaize the tracking tables from first observation
+    T1[:, 0] = Pi * B[:, y[0]]
+    T2[:, 0] = 0
+
+    # Iterate throught the observations updating the tracking tables
+    for i in range(1, T):
+        T1[:, i] = np.max(T1[:, i - 1] * A.T * B[np.newaxis, :, y[i]].T, 1)
+        T2[:, i] = np.argmax(T1[:, i - 1] * A.T, 1)
+
+    # Build the output, optimal model trajectory
+    x = np.empty(T, 'B')
+    x[-1] = np.argmax(T1[:, T - 1])
+    for i in reversed(range(1, T)):
+        x[i - 1] = T2[x[i], i]
+
+    return x, T1, T2
+
 
 
 if __name__ == '__main__':
