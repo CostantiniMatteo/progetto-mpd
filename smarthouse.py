1bd6ffc4ae0fc4e6958d4ab474d90297e978e303
Matteo Costantini <matteo.angelo.costantini@gmail.com>
2018-06-12 11:48:24 +0200

    Fix matrice di transizione e emissione

--- smarthouse.py
+++ smarthouse.py
@@ -15,6 +15,9 @@ def probability_distribution(seq1, seq2):
     for s1, s2 in zip(seq1, seq2):
         M[s1][s2] += 1
 
+    # Pone a 'epsilon' le probabilità che sono zero
+    M[M == 0] = 10e-2
+
     # Calcolo delle distribuzioni di probabilità
     row_sums = M.sum(axis=1)
     M = M / row_sums[:, np.newaxis]
@@ -46,6 +49,53 @@ def date_to_timestamp(m):
 def print_numpy_matrix(m):
     import sys
     np.savetxt(sys.stdout, m, '%6.4f')
+
+
+def viterbi(initial, transition, emission, events):
+    """Find the likeliest path in a hidden Markov Model resulting in the
+    given events.
+
+    Arguments:
+    initial: arraylike(n) --- probability of starting in each state
+    transition: arraylike(n, n) -- probability of transition between states
+    emission: arraylike(n, e) -- probability of emitting each event in
+        each state
+    events -- iterable of events
+
+    Returns:
+    path: list(int) -- list of states in the most probable path
+    p: float -- log-likelihood of that path
+
+    """
+    # Use log-likelihoods to avoid floating-point underflow. Note that
+    # we want -inf for the log of zero, so suppress warnings here.
+    with np.errstate(divide='ignore'):
+        initial = np.log10(initial)
+        transition = np.log10(transition)
+        emission = np.log10(emission)
+
+
+    # List of arrays giving most likely previous state for each state.
+    prev = []
+
+    events = iter(events)
+    logprob = initial + emission[:, next(events)]
+    for event in events:
+        # p[i, j] is log-likelihood of being in state j, having come from i.
+        p = logprob[:, np.newaxis] + transition + emission[:, event]
+        prev.append(np.argmax(p, axis=0))
+        logprob = np.max(p, axis=0)
+
+    # Most likely final state.
+    best_state = np.argmax(logprob)
+
+    # Reconstruct path by following links and then reversing.
+    state = best_state
+    path = [state]
+    for p in reversed(prev):
+        state = p[state]
+        path.append(state)
+    return path[::-1], logprob[best_state]
 
 
 # Genera una sequenza di stati e di osservazioni campionando utilizzando le
@@ -121,75 +171,31 @@ def main(train_rate=0.75, to_date=None, n_samples=0, length=None):
             if i == j:
                 c += 1
         print(f"Dataset {f}, trainset: {size}: {c/len(seq):.3f}")
+        # print(seq)
         res.append(c/len(seq))
 
     return res
 
 
-def viterbi(initial, transition, emission, events):
-    """Find the likeliest path in a hidden Markov Model resulting in the
-    given events.
-
-    Arguments:
-    initial: arraylike(n) --- probability of starting in each state
-    transition: arraylike(n, n) -- probability of transition between states
-    emission: arraylike(n, e) -- probability of emitting each event in
-        each state
-    events -- iterable of events
-
-    Returns:
-    path: list(int) -- list of states in the most probable path
-    p: float -- log-likelihood of that path
-
-    """
-    # Use log-likelihoods to avoid floating-point underflow. Note that
-    # we want -inf for the log of zero, so suppress warnings here.
-    with np.errstate(divide='ignore'):
-        initial = np.log10(initial)
-        transition = np.log10(transition)
-        emission = np.log10(emission)
-
-    # List of arrays giving most likely previous state for each state.
-    prev = []
-
-    events = iter(events)
-    logprob = initial + emission[:, next(events)]
-    for event in events:
-        # p[i, j] is log-likelihood of being in state j, having come from i.
-        p = logprob[:, np.newaxis] + transition + emission[:, event]
-        prev.append(np.argmax(p, axis=0))
-        logprob = np.max(p, axis=0)
-
-    # Most likely final state.
-    best_state = np.argmax(logprob)
-
-    # Reconstruct path by following links and then reversing.
-    state = best_state
-    path = [state]
-    for p in reversed(prev):
-        state = p[state]
-        path.append(state)
-    return path[::-1], logprob[best_state]
-
-
 if __name__ == '__main__':
-    # main(n_samples=10000)
-    import matplotlib.pyplot as plt
-    xs = []; ys = []
+    main(n_samples=10000, length=600)
+    # import matplotlib.pyplot as plt
+    # xs = []; ys = []
+
 
     # Plot dell'accuracy facendo variare il trainset in giorni
-    start_A = date_to_timestamp("2011-11-28 00:00:00")
-    start_B = date_to_timestamp("2012-11-11 00:00:00")
-    for i in range(7, 14):
-        d = {'A': start_A + 86400*i , 'B': start_B + 86400*i}
-        try:
-            res = main(to_date=d)
-            ys.append(res)
-            xs.append(i)
-            print("Done", i)
-        except:
-            pass
+    # start_A = date_to_timestamp("2011-11-28 00:00:00")
+    # start_B = date_to_timestamp("2012-11-11 00:00:00")
+    # for i in range(7, 14):
+    #     d = {'A': start_A + 86400*i , 'B': start_B + 86400*i}
+    #     try:
+    #         res = main(to_date=d)
+    #         ys.append(res)
+    #         xs.append(i)
+    #         print("Done", i)
+    #     except:
+    #         pass
 
-    plt.plot(xs, [y_i[0] for y_i in ys])
-    plt.plot(xs, [y_i[1] for y_i in ys])
-    plt.show()
+    # plt.plot(xs, [y_i[0] for y_i in ys])
+    # plt.plot(xs, [y_i[1] for y_i in ys])
+    # plt.show()
